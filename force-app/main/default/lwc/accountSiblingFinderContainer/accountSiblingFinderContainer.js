07782242d0519889718c819efb451d04b3ecbd79
Mitch Long <mitchlong@protonmail.com>
2022-12-01 19:36:08 -0800

    Adds references for fields to display in data table & empty form submission handler methods

--- force-app/main/default/lwc/accountSiblingFinderContainer/accountSiblingFinderContainer.js
+++ force-app/main/default/lwc/accountSiblingFinderContainer/accountSiblingFinderContainer.js
@@ -1,6 +1,16 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
+
+import ACCOUNT_NAME_FIELD from '@salesforce/schema/Account.Name';
+import CONTACT_NAME_FIELD from '@salesforce/schema/Contact.Name';
+import CONTACT_EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+import CONTACT_PHONE_FIELD from '@salesforce/schema/Contact.MobilePhone';
+import CONTACT_STATUS_FIELD from '@salesforce/schema/Contact.Status__c';
+import TAX_ID_FIELD from '@salesforce/schema/Contact.Tax_Id__c';
 
 export default class AccountSiblingFinderContainer extends LightningElement {
 
-    
+
+    handleFormInputChange(event) {}
+
+    handleGetSiblingAccounts(event) {}
 }
