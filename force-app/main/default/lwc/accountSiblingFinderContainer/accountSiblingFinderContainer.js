b2467dbfbf18c2a43e7856a90a1ec420a70b56fe
Mitchell  Long <meechll@mitchells-mbp.lan>
2022-12-01 19:03:09 -0800

    Adds form input, label and search button

--- force-app/main/default/lwc/accountSiblingFinderContainer/accountSiblingFinderContainer.js
+++ force-app/main/default/lwc/accountSiblingFinderContainer/accountSiblingFinderContainer.js
@@ -1,3 +1,6 @@
 import { LightningElement } from 'lwc';
 
-export default class AccountSiblingFinderContainer extends LightningElement {}
+export default class AccountSiblingFinderContainer extends LightningElement {
+
+    
+}
